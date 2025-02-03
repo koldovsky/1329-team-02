136ffd6f77f8df58fbcfbc1ce82714d185a1f54f
prohodec <kopitin112@gmail.com>
2025-02-03 18:55:43 +0200

    add buttons for preview episodes

--- js/index.broadcasts.js
+++ js/index.broadcasts.js
@@ -1,32 +1,23 @@
-let allowAudio = false;
+const podcastCards = document.querySelectorAll('.podcast-card');
 
-const buttonAllowAudio = document.querySelector('.button-allow-audio');
-buttonAllowAudio.addEventListener('click', () => {
-  if (allowAudio === false) {
-    allowAudio = true;
-    buttonAllowAudio.innerHTML =
-      'Mute Audio <svg class="button-allow-audio__icon" width="16px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-whiteidth="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12.1657 2.14424C12.8728 2.50021 13 3.27314 13 3.7446V20.2561C13 20.7286 12.8717 21.4998 12.1656 21.8554C11.416 22.2331 10.7175 21.8081 10.3623 21.4891L4.95001 16.6248H3.00001C1.89544 16.6248 1.00001 15.7293 1.00001 14.6248L1 9.43717C1 8.3326 1.89543 7.43717 3 7.43717H4.94661L10.3623 2.51158C10.7163 2.19354 11.4151 1.76635 12.1657 2.14424Z" fill="white"></path> <path d="M21.8232 15.6767C21.4327 16.0673 20.7995 16.0673 20.409 15.6768L18.5 13.7678L16.591 15.6768C16.2005 16.0673 15.5673 16.0673 15.1768 15.6767L14.8233 15.3232C14.4327 14.9327 14.4327 14.2995 14.8233 13.909L16.7322 12L14.8232 10.091C14.4327 9.70044 14.4327 9.06727 14.8232 8.67675L15.1767 8.3232C15.5673 7.93267 16.2004 7.93267 16.591 8.32319L18.5 10.2322L20.409 8.32319C20.7996 7.93267 21.4327 7.93267 21.8233 8.3232L22.1768 8.67675C22.5673 9.06727 22.5673 9.70044 22.1768 10.091L20.2678 12L22.1767 13.909C22.5673 14.2995 22.5673 14.9327 22.1767 15.3232L21.8232 15.6767Z" fill="white"></path> </g></svg>';
-  } else {
-    allowAudio = false;
-    buttonAllowAudio.innerHTML =
-      'Allow Audio <svg class="button-allow-audio__icon" width="16px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="white"><g id="SVGRepo_bgCarrier" stroke-whiteidth="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M13 3.7446C13 3.27314 12.8728 2.50021 12.1657 2.14424C11.4151 1.76635 10.7163 2.19354 10.3623 2.51158L4.94661 7.43717H3C1.89543 7.43717 1 8.3326 1 9.43717L1.00001 14.6248C1.00001 15.7293 1.89544 16.6248 3.00001 16.6248H4.95001L10.3623 21.4891C10.7175 21.8081 11.416 22.2331 12.1656 21.8554C12.8717 21.4998 13 20.7286 13 20.2561V3.7446Z" fill="white"></path> <path d="M17.336 3.79605L17.0952 3.72886C16.5633 3.58042 16.0117 3.89132 15.8632 4.42329L15.7289 4.90489C15.5804 5.43685 15.8913 5.98843 16.4233 6.13687L16.6641 6.20406C18.9551 6.84336 20.7501 9.14615 20.7501 12.0001C20.7501 14.854 18.9551 17.1568 16.6641 17.7961L16.4233 17.8632C15.8913 18.0117 15.5804 18.5633 15.7289 19.0952L15.8632 19.5768C16.0117 20.1088 16.5633 20.4197 17.0952 20.2713L17.336 20.2041C20.7957 19.2387 23.2501 15.8818 23.2501 12.0001C23.2501 8.11832 20.7957 4.76146 17.336 3.79605Z" fill="white"></path> <path d="M16.3581 7.80239L16.1185 7.73078C15.5894 7.57258 15.0322 7.87329 14.874 8.40243L14.7308 8.88148C14.5726 9.41062 14.8733 9.96782 15.4024 10.126L15.642 10.1976C16.1752 10.3571 16.75 11.012 16.75 12C16.75 12.9881 16.1752 13.643 15.642 13.8024L15.4024 13.874C14.8733 14.0322 14.5726 14.5894 14.7308 15.1185L14.874 15.5976C15.0322 16.1267 15.5894 16.4274 16.1185 16.2692L16.3581 16.1976C18.1251 15.6693 19.25 13.8987 19.25 12C19.25 10.1014 18.1251 8.33068 16.3581 7.80239Z" fill="white"></path> </g></svg>';
-  }
-});
+podcastCards.forEach(card => {
+  const audioSrc = card.getAttribute('data-audio');
+  const button = card.querySelector('.preview-button');
 
-document.querySelectorAll('.podcast-card').forEach(card => {
-  const audio = new Audio(card.dataset.audio);
-  audio.volume = 0.3;
+  let audio = new Audio(audioSrc);
 
-  card.addEventListener('mouseenter', () => {
-    if (allowAudio && window.innerWidth >= 1280) {
+  button.addEventListener('click', function () {
+    if (audio.paused) {
+      audio.play();
+      button.innerHTML = 'Preview Episode <svg width="13px" viewBox="0 -0.5 17 17" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="preview-button__pause-icon" fill="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>1229</title> <defs> </defs> <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g transform="translate(1.000000, 0.000000)" fill="#ffffff"> <path d="M0.002,1 C0.002,0.447 0.446,0 0.995,0 L4.967,0 C5.516,0 5.96,0.447 5.96,1 L5.96,15 C5.96,15.553 5.516,16 4.967,16 L0.995,16 C0.446,16 0.002,15.553 0.002,15 L0.002,1 L0.002,1 Z" class="si-glyph-fill"> </path> <path d="M8.002,1 C8.002,0.447 8.446,0 8.995,0 L12.967,0 C13.516,0 13.96,0.447 13.96,1 L13.96,15 C13.96,15.553 13.516,16 12.967,16 L8.995,16 C8.446,16 8.002,15.553 8.002,15 L8.002,1 L8.002,1 Z" > </path> </g> </g> </g></svg>';
+    } else {
+      audio.pause();
       audio.currentTime = 0;
-      audio.play().catch(error => console.error('autoplay blocked:', error));
+      button.innerHTML = 'Preview Episode <svg class="podcast-card__button-icon"xmlns="http://www.w3.org/2000/svg"viewBox="0 0 1000 1000"><path d="M732.5,508c0,15.119-8.137,28.332-20.275,35.517L410.207,721.984c-0.107,0.067-0.215,0.121-0.309,0.188l-0.174,0.095c-6.137,3.639-13.32,5.733-20.975,5.733c-22.773,0-41.25-18.463-41.25-41.25v-357.5c0-22.773,18.477-41.25,41.25-41.25c7.654,0,14.838,2.108,20.975,5.747v-0.014l0.174,0.094c0.094,0.081,0.201,0.134,0.309,0.202l302.018,178.454C724.363,479.681,732.5,492.894,732.5,508"></path></svg>';
     }
   });
 
-  card.addEventListener('mouseleave', () => {
-    if (allowAudio && window.innerWidth >= 1280) {
-      audio.pause();
-    }
+  audio.addEventListener('ended', function () {
+    button.innerHTML = 'Preview Episode <svg class="podcast-card__button-icon"xmlns="http://www.w3.org/2000/svg"viewBox="0 0 1000 1000"><path d="M732.5,508c0,15.119-8.137,28.332-20.275,35.517L410.207,721.984c-0.107,0.067-0.215,0.121-0.309,0.188l-0.174,0.095c-6.137,3.639-13.32,5.733-20.975,5.733c-22.773,0-41.25-18.463-41.25-41.25v-357.5c0-22.773,18.477-41.25,41.25-41.25c7.654,0,14.838,2.108,20.975,5.747v-0.014l0.174,0.094c0.094,0.081,0.201,0.134,0.309,0.202l302.018,178.454C724.363,479.681,732.5,492.894,732.5,508"></path></svg>';
   });
 });
